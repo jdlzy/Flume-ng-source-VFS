8b5decd76c5abcbd96225f1b77e8aff050e0921c
liuziyang <616560426@qq.com>
2018-04-11 22:51:53 +0800

    commit

--- src/main/scala/org/keedio/flume/source/vfs/source/SourceVFS.scala
+++ src/main/scala/org/keedio/flume/source/vfs/source/SourceVFS.scala
@@ -207,6 +207,7 @@ class SourceVFS extends AbstractSource with Configurable with EventDrivenSource 
     val headers: java.util.Map[String, String] = new util.HashMap[String, String]()
     headers.put("timestamp", String.valueOf(System.currentTimeMillis()));
     headers.put("fileName", fileName);
+    headers.put("flag","LTE_SITE")
     event.setBody(data)
     event.setHeaders(headers)
     try {
